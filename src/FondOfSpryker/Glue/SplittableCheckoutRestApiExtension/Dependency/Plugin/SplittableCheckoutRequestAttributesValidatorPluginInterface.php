e4e24b56a69e7c7579bedf8dc67fe892ad659402
Jozsef Geng <jozsef.geng@fondof.de>
2020-09-09 10:41:31 +0200

    add SplittableCheckoutRequestAttributesValidatorPluginInterface

---
+++ src/FondOfSpryker/Glue/SplittableCheckoutRestApiExtension/Dependency/Plugin/SplittableCheckoutRequestAttributesValidatorPluginInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FondOfSpryker\Glue\SplittableCheckoutRestApiExtension\Dependency\Plugin;
+
+use Generated\Shared\Transfer\RestCheckoutRequestAttributesTransfer;
+use Generated\Shared\Transfer\RestErrorCollectionTransfer;
+use Generated\Shared\Transfer\RestSplittableCheckoutRequestAttributesTransfer;
+
+interface SplittableCheckoutRequestAttributesValidatorPluginInterface
+{
+    /**
+     * Specification:
+     * - Validates splittable checkout Rest API request attributes.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\RestSplittableCheckoutRequestAttributesTransfer $splittableCheckoutRequestAttributesTransfer
+     *
+     * @return \Generated\Shared\Transfer\RestErrorCollectionTransfer
+     */
+    public function validateAttributes(
+        RestSplittableCheckoutRequestAttributesTransfer $splittableCheckoutRequestAttributesTransfer
+    ): RestErrorCollectionTransfer;
+}
